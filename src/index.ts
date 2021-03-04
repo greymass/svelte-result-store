e9af82b6a12e11d4de61565f704dd0f14ca559fd
Johan Nordberg <its@johan-nordberg.com>
2021-03-04 12:16:35 +0900

    Implement result store

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,168 @@
-/**
- * Solve for completeness.
- * @param n The number.
- * @param p The problem.
- * @param hard Set to true for super hard problem.
- * @returns The solution.
- */
-export function maths(n: number, p: number, hard = false) {
-    while (hard) {
-        n = n * p
+import {derived as svelteDerived, readable as svelteReadable} from 'svelte/store'
+import type {Readable} from 'svelte/store'
+
+export type Result<T> = {value?: T; error?: Error}
+
+type Subscriber<T> = (value: T) => void
+type Unsubscriber = () => void
+type Invalidator<T> = (value?: T) => void
+type StartStopNotifier<T> = (set: Subscriber<T>, error: Subscriber<Error>) => Unsubscriber | void
+type Stores = Readable<Result<any>> | [Readable<Result<any>>, ...Array<Readable<Result<any>>>]
+type StoresValues<T> = T extends Readable<Result<infer U>>
+    ? U
+    : {
+          [K in keyof T]: T[K] extends Readable<Result<infer U>> ? U : never
+      }
+
+class ResultReadable<T> implements Readable<Result<T>> {
+    private valueStore?: Readable<T | undefined>
+    private errorStore?: Readable<Error | undefined>
+    private resolvedStore?: Readable<boolean>
+
+    constructor(private resultStore: Readable<Result<T>>) {}
+
+    subscribe(run: Subscriber<Result<T>>, invalidate?: Invalidator<Result<T>>): Unsubscriber {
+        return this.resultStore.subscribe(run, invalidate)
     }
-    return n ^ p
+
+    /**
+     * A store containing the value or undefined if there is an error.
+     * Can also be undefined for async stores while the value is resolved.
+     */
+    get value(): Readable<T | undefined> {
+        if (!this.valueStore) {
+            this.valueStore = svelteDerived(this.resultStore, (result, set) => {
+                if (result.error === undefined) {
+                    set(result.value)
+                }
+            })
+        }
+        return this.valueStore
+    }
+
+    /**
+     * A store containing the error or undefined.
+     */
+    get error(): Readable<Error | undefined> {
+        if (!this.errorStore) {
+            this.errorStore = svelteDerived(this.resultStore, (result, set) => {
+                if (result.error !== undefined) {
+                    set(result.error)
+                }
+            })
+        }
+        return this.errorStore
+    }
+
+    /**
+     * A store containing true if the readable has a result (a value or error), false otherwise.
+     */
+    get resolved(): Readable<boolean> {
+        if (!this.resolvedStore) {
+            this.resolvedStore = svelteDerived(
+                this.resultStore,
+                (result, set) => {
+                    set(result.error !== undefined || result.value !== undefined)
+                },
+                false as boolean
+            )
+        }
+        return this.resolvedStore
+    }
+
+    /** A promise that resolves or rejects on the first value or error. */
+    get promise(): Promise<T> {
+        return new Promise((resolve, reject) => {
+            const done = this.resultStore.subscribe((result) => {
+                if (result.error !== undefined) {
+                    reject(result.error)
+                } else if (result.value !== undefined) {
+                    resolve(result.value)
+                    done()
+                }
+            })
+        })
+    }
+}
+
+export function readable<T>(start: StartStopNotifier<T> | (() => Promise<T>)): ResultReadable<T> {
+    const result: Result<T> = {}
+    const readable = svelteReadable(result, (setResult) => {
+        try {
+            const rv = start(
+                (value) => {
+                    setResult({value})
+                },
+                (error) => {
+                    setResult({error})
+                }
+            )
+            if (rv instanceof Promise) {
+                rv.then((value) => {
+                    setResult({value})
+                }).catch((error) => {
+                    setResult({error})
+                })
+            } else {
+                return rv
+            }
+        } catch (error) {
+            setResult({error})
+        }
+    })
+    return new ResultReadable(readable)
+}
+
+type DerivedStartStopNotifier<S extends Stores, T> = (
+    values: StoresValues<S>,
+    set: (value: T) => void,
+    error: (error: Error) => void
+) => Unsubscriber | void
+
+type DerivedPromise<S extends Stores, T> = (values: StoresValues<S>) => Promise<T>
+
+/** Like svelte/store's derived but acts only when all results have resolved to a value. */
+export function derived<S extends Stores, T>(
+    stores: S,
+    fn: DerivedStartStopNotifier<S, T> | DerivedPromise<S, T>
+): ResultReadable<T> {
+    const single = !Array.isArray(stores)
+    const store = svelteDerived<S, Result<T>>(stores, (storeValues, set) => {
+        const results = single
+            ? [storeValues as StoresValues<S>]
+            : (storeValues as StoresValues<S>[])
+        const error = results.find((r) => r.error !== undefined)
+        if (error) {
+            set(error)
+        } else {
+            const values = results.map((r) => r.value)
+            if (values.every((v) => v !== undefined)) {
+                try {
+                    const rv = fn(
+                        values as StoresValues<S>,
+                        (value) => {
+                            set({value})
+                        },
+                        (error) => {
+                            set({error})
+                        }
+                    )
+                    if (rv instanceof Promise) {
+                        rv.then((value) => {
+                            set({value})
+                        }).catch((error) => {
+                            set({error})
+                        })
+                    } else {
+                        return rv
+                    }
+                } catch (error) {
+                    set({error})
+                }
+            } else {
+                set({})
+            }
+        }
+    })
+    return new ResultReadable(store)
 }
--- test/index.ts
+++ test/index.ts
@@ -1,10 +1,263 @@
 import {strict as assert} from 'assert'
 import 'mocha'
 
-import * as pkg from '../src'
+import {derived, readable} from '../src'
 
-suite('index', function () {
-    test('maths', function () {
-        assert.equal(pkg.maths(1, 2), 3)
+suite('result store', function () {
+    test('subscribe', function (done) {
+        const store = readable<number>((set) => {
+            set(42)
+        })
+        store.subscribe((value) => {
+            assert.equal(value.value, 42)
+            assert.equal(value.error, undefined)
+            done()
+        })
+    })
+
+    test('derived value', function (done) {
+        const store = readable<number>((set) => {
+            set(42)
+        })
+        store.error.subscribe((error) => {
+            assert.equal(error, undefined)
+        })
+        store.value.subscribe((value) => {
+            assert.equal(value, 42)
+            done()
+        })
+    })
+
+    test('derived error', function (done) {
+        const store = readable<number>((set, error) => {
+            error(new Error('fail'))
+        })
+        store.value.subscribe((value) => {
+            assert.equal(value, undefined)
+        })
+        store.error.subscribe((error) => {
+            assert.equal(String(error), 'Error: fail')
+            done()
+        })
+    })
+
+    test('throwing', function (done) {
+        const store = readable<number>(() => {
+            throw new Error('fail')
+        })
+        store.error.subscribe((error) => {
+            assert.equal(String(error), 'Error: fail')
+            done()
+        })
+    })
+
+    test('delayed', function (done) {
+        const store = readable<number>((set) => {
+            setTimeout(() => {
+                set(100)
+            }, 10)
+        })
+        let update = 0
+        store.value.subscribe((value) => {
+            update++
+            switch (update) {
+                case 1:
+                    assert.equal(value, undefined)
+                    break
+                case 2:
+                    assert.equal(value, 100)
+                    done()
+                    break
+                default:
+                    assert.fail()
+            }
+        })
+    })
+
+    test('async', function (done) {
+        const store = readable<number>(async () => {
+            await sleep(10)
+            return 42
+        })
+        let update = 0
+        store.value.subscribe((value) => {
+            update++
+            switch (update) {
+                case 1:
+                    assert.equal(value, undefined)
+                    break
+                case 2:
+                    assert.equal(value, 42)
+                    done()
+                    break
+                default:
+                    assert.fail()
+            }
+        })
+    })
+
+    test('read async throwing', function (done) {
+        const store = readable<number>(async () => {
+            throw new Error('fail')
+        })
+        store.error.subscribe((error) => {
+            if (error) {
+                assert.equal(String(error), 'Error: fail')
+                done()
+            }
+        })
+    })
+
+    test('derived', function (done) {
+        const a = readable<number>((set) => {
+            set(1)
+        })
+        const b = readable<number>((set) => {
+            setTimeout(() => {
+                set(2)
+            }, 10)
+        })
+        const store = derived([a, b], ([va, vb], set) => {
+            set(va + vb)
+        })
+        store.value.subscribe((v) => {
+            if (v) {
+                assert.equal(v, 3)
+                done()
+            }
+        })
+    })
+
+    test('derived upstream error', function (done) {
+        const a = readable<number>((set) => {
+            set(1)
+        })
+        const b = readable<number>(() => {
+            throw new Error('fail')
+        })
+        const store = derived([a, b], ([va, vb], set) => {
+            set(va + vb)
+        })
+        store.error.subscribe((error) => {
+            if (error) {
+                assert.equal(String(error), 'Error: fail')
+                done()
+            }
+        })
+    })
+
+    test('derived error', function (done) {
+        const a = readable<number>((set) => {
+            set(1)
+        })
+        const b = readable<number>((set) => {
+            set(2)
+        })
+        const store = derived([a, b], ([va, vb], set, error) => {
+            set(va + vb)
+            setTimeout(() => {
+                error(new Error('fail'))
+            }, 10)
+        })
+        store.error.subscribe((error) => {
+            if (error) {
+                assert.equal(String(error), 'Error: fail')
+                done()
+            }
+        })
+    })
+
+    test('derived throwing', function (done) {
+        const a = readable<number>((set) => {
+            set(1)
+        })
+        const b = readable<number>((set) => {
+            setTimeout(() => {
+                set(2)
+            }, 10)
+        })
+        const store = derived([a, b], () => {
+            throw new Error('fail')
+        })
+        store.error.subscribe((error) => {
+            if (error) {
+                assert.equal(String(error), 'Error: fail')
+                done()
+            }
+        })
+    })
+
+    test('derived async', function (done) {
+        const a = readable<number>((set) => {
+            set(1)
+        })
+        const b = readable<number>(async () => {
+            await sleep(1)
+            return 2
+        })
+        const store = derived([a, b], async ([va, vb]) => {
+            return va + vb
+        })
+        store.value.subscribe((v) => {
+            if (v) {
+                assert.equal(v, 3)
+                done()
+            }
+        })
+    })
+
+    test('derived async throwing', function (done) {
+        const a = readable<number>(async () => {
+            await sleep(1)
+            return 2
+        })
+        const store = derived(a, async () => {
+            throw new Error('fail')
+        })
+        store.error.subscribe((error) => {
+            if (error) {
+                assert.equal(String(error), 'Error: fail')
+                done()
+            }
+        })
+    })
+
+    test('result promise', async function () {
+        let error: any
+        const store = readable<number>((set, e) => {
+            set(123)
+            error = e
+        })
+        const result = await store.promise
+        assert.equal(result, 123)
+        error(new Error('fail'))
+        assert.rejects(async () => {
+            await store.promise
+        })
+    })
+
+    test('result resolved', function (done) {
+        let set: any
+        const store = readable<number>((s) => {
+            set = s
+        })
+        let n = 0
+        store.resolved.subscribe((v) => {
+            n++
+            if (n === 1) {
+                assert.equal(v, false)
+                set(1)
+                store.resolved.subscribe((v) => {
+                    assert.equal(v, true)
+                    done()
+                })
+            }
+        })
     })
 })
+
+function sleep(ms: number) {
+    return new Promise<void>((resolve) => {
+        setTimeout(resolve, ms)
+    })
+}
